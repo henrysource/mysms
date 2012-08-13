4994e357c9d711419b67a2024b28c9fe7d2dde07
HenrySource <hst1805@gmail.com>
2012-08-13 13:21:37 +0800

    mysms initialize method

--- lib/mysms.rb
+++ lib/mysms.rb
@@ -1,7 +1,28 @@
 require "mysms/version"
+require "socket"
 
 module Mysms
-  def test
-  	
-  end
+	class Client
+		def initialize(key,secret)
+			@key,@secret = key, secret # cid,spw
+			@host = "mysms.com.tw"
+			@port = "80"
+
+			s = TCPSocket.open(host,port)
+
+			while line = s.gets
+				puts line.chop
+			end
+
+			s.close
+		end
+
+		attr_accessor :key,:secret,:host,:port
+
+		def send_message(data)
+#			response = @http.post('/mysms/ApSendSMS.php',encode(data),headers)	
+		end
+
+	end
+
 end
